803163823f746e3765299d812f14ec50bf39db03
Gaspard Hoyaux <ghoyaux@c1r6p2.42lehavre.fr>
2024-08-24 18:32:14 +0200

    Struct added

--- srcs/base.c
+++ srcs/base.c
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rush02.h                                        :+:      :+:    :+:   */
+/*   base.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ghoyaux <ghoyaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/24 15:16:07 by ghoyaux           #+#    #+#             */
-/*   Updated: 2024/08/24 18:31:45 by ghoyaux          ###   ########.fr       */
+/*   Created: 2024/08/24 16:57:27 by ghoyaux           #+#    #+#             */
+/*   Updated: 2024/08/24 18:10:08 by ghoyaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Fonctions autorisee : write, malloc, free, open, read, close */
-#include <unistd.h>
-#include <stdlib.h>
-
-typedef struct s_dict_array
+char	*ft_create_array(char *str)
 {
-	int		size;
-	char	*numbers;
-	char	*letters;
-}	t_dict_array;
-
-/* Declarations of functions utils.c */
-int	ft_is_digit(char *str);
-int	ft_get_sign(char *str);
-int	ft_convert_char(char *str);
-int	ft_strlen(char *str);
+	
+	return (array);
+}
